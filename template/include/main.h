9a1b5b9f4ca953287113b1e66df9378fbe26a4b0
Tanmay Mudholkar <tkmudholkar@gmail.com>
2018-07-19 23:57:33 +0200

    Added progress bar test to template.

--- template/include/main.h
+++ template/include/main.h
@@ -1,3 +1,6 @@
 #include <iostream>
+#include <chrono>
+#include <thread>
 
 #include "tmArgumentParser.h"
+#include "tmProgressBar.h"
--- template/src/main.cpp
+++ template/src/main.cpp
@@ -2,11 +2,31 @@
 
 int main(int argc, char* argv[]) {
   std::cout << "Hello world!" << std::endl;
+
+  std::cout << "Constructing example argument parser..." << std::endl;
   tmArgumentParser argumentParser = tmArgumentParser("Template parser.");
   argumentParser.addArgument("arg", "defaultValue", false, "argument example.");
   argumentParser.setPassedStringValues(argc, argv);
 
   std::string argValue = argumentParser.getArgumentString("arg");
   std::cout << "Argument \"arg\" is set to \"" << argValue << "\"" << std::endl;
+
+  std::cout << "Constructing example progress bar without counter..." << std::endl;
+  tmProgressBar* progressBarNoCounter = new tmProgressBar();
+  progressBarNoCounter->initialize();
+  for (double fractionCompleted = 0; fractionCompleted <= 1; fractionCompleted += 0.002) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    progressBarNoCounter->updateBar(fractionCompleted);
+  }
+  delete progressBarNoCounter;
+  
+  std::cout << "Constructing example progress bar with counter, without initializing. Should print a warning..." << std::endl;
+  tmProgressBar* progressBar = new tmProgressBar(10000);
+  for (int testCounter = 0; testCounter < 10000; ++testCounter) {
+    double fractionCompleted = 1.0*testCounter/10000;
+    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    progressBar->updateBar(fractionCompleted, testCounter);
+  }
+  delete progressBar;
   return 0;
 }
